b440f29010acc201dc22afaee075339b9c5a7bb0
sugimoto <c0b2118330@edu.teu.ac.jp>
2022-06-14 18:20:58 +0900

    アルファベットゲーム

--- ex01/alphabet.py
+++ ex01/alphabet.py
@@ -29,16 +29,7 @@ def ans(kotae):
     num = int(input("欠損文字はいくつあるでしょうか"))
     if num != kaisu:
         print("不正解")
-        print("-"*50)
         return 0 
-    else:
-        print("正解です")
-        for i in range(kaisu)
-            c = input(f"{i+1}つ目の文字を入力")
-            if c not in kotae:
-                print("不正解")
-                print("-"*50)
-                return 0 
-            kotae.remove(c)
-        print("正解　ゲーム終了")
-        return 1
+    
+ if __name__ ==  "__main__"
+    main()
