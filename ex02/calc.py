8f3d1137c32d81a61973cdd30e579e243f78649d
sugimoto <c0b2118330@edu.teu.ac.jp>
2022-06-21 17:25:15 +0900

    修正

--- ex02/calc.py
+++ ex02/calc.py
@@ -22,7 +22,6 @@ def button_click(event):
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("電卓")
-    #root.geometry("300x500")
 
     entry= tk.Entry(root,justify="right",width=10,font=("Times New Roman",40))
     entry.grid(row=0,column=0,columnspan=3)
