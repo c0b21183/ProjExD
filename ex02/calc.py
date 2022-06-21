143fa204280492c82ea47a522682a9219aedbca9
sugimoto <c0b2118330@edu.teu.ac.jp>
2022-06-21 18:07:50 +0900

    修正

--- ex02/calc.py
+++ ex02/calc.py
@@ -4,7 +4,7 @@ import tkinter.messagebox as tkm
 
 def C(event):
     if num == "C":
-        entry.delete(0,tk.END)
+        entry.delete(0, tk.END)
 
 def button_click(event):
     btn=event.widget
@@ -12,7 +12,7 @@ def button_click(event):
     if num == "=":
         eqn = entry.get()
         res = eval(eqn)
-        entry.delete(0,tk.END)
+        entry.delete(0, tk.END)
         entry.insert(tk.END,res)
 
     else:
@@ -22,20 +22,19 @@ def button_click(event):
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("電卓")
-    #root.geometry("300x500")
 
     entry= tk.Entry(root,justify="right",width=10,font=("Times New Roman",40))
     entry.grid(row=0,column=0,columnspan=3)
 
 
     r, c= 1, 0
-    for i,num in enumerate([9,8,7,6,5,4,3,2,1,0,"+","="]):
+    for i,num in enumerate([9, 8, 7, 6, 5, 4, 3, 2, 1, 0, "+", "="]):
         btn = tk.Button(root,
                         text=f"{num}",
                         width=4, 
                         height=2,
                         bg="#00ffff",
-                        font=("Times New Roman",30)
+                        font=("Times New Roman", 30)
                         
                     )
         btn.bind("<1>",button_click)
